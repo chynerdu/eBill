4e6a042cf650aabeb856135e899b84a520d7b7eb
Chinedu <chinedu@beyondit.it>
2023-12-09 03:20:21 +0100

    Added mmore test to assert Navigation WIP

--- eBill.Tests/Bill.Test.cs
+++ eBill.Tests/Bill.Test.cs
@@ -9,7 +9,7 @@ namespace eBill.Tests
     public class BillTest: IDisposable
     {
     private IWebDriver driver;
-    private const string Url = "http://localhost:5000";
+    private const string Url = "http://localhost:5000/";
 
        
         public  BillTest()
@@ -44,7 +44,7 @@ namespace eBill.Tests
     }
 
         [Fact]
-        public void checkIfAllBillsShowRightTitle()
+        public void CheckIfAllBillsShowRightTitle()
     {
         // Navigate to the web page
          driver.Navigate().GoToUrl("http://localhost:5000/Home/AllBills");
@@ -55,6 +55,44 @@ namespace eBill.Tests
         Assert.Equal("Showing all bills", element.Text);
 
     }
+
+        [Fact]
+        public void ShouldNavigateToAllBillsOnSubmit()
+    {
+        driver.Navigate().GoToUrl(Url);
+        IWebElement name =  driver.FindElement(By.Id("Name"));
+        name.SendKeys("Dinner with friends");
+
+        IWebElement amount =  driver.FindElement(By.Id("Amount"));
+        amount.SendKeys("200");
+
+         IWebElement DueDate =  driver.FindElement(By.Id("DueDate"));
+        DueDate.SendKeys(DateTime.Now.ToString("yyyy-MM-ddTHH:mm"));
+ 
+        IWebElement submitButton =  driver.FindElement(By.ClassName("btn-primary"));
+        submitButton.Click();
+        // Wait for the page to load (you might need to adjust the wait time based on your application)
+        System.Threading.Thread.Sleep(5000);
+        string urlAfterButtonClick = driver.Url;
+        // Assert that the element is not null, indicating that it exists
+        Assert.Equal("http://localhost:5000/Home/AllBills", urlAfterButtonClick);
+
+    }
+
+    [Fact]
+        public void ShouldNotNavigateOnEmptyInput()
+    {
+         driver.Navigate().GoToUrl(Url);
+        IWebElement element =  driver.FindElement(By.ClassName ("btn-primary"));
+        element.Click();
+        // Wait for the page to load (you might need to adjust the wait time based on your application)
+        System.Threading.Thread.Sleep(2000);
+        string urlAfterButtonClick = driver.Url;
+        // Assert that the element is not null, indicating that it exists
+        Assert.Equal(Url, urlAfterButtonClick);
+
+    }
+
 
         [Fact]
         public void Dispose()
