7077ffeb0533600b521771831fa937f45d48afdb
Chinedu <chinedu@beyondit.it>
2023-12-09 02:50:01 +0100

    Added some tests

--- eBill.Tests/Bill.Test.cs
+++ eBill.Tests/Bill.Test.cs
@@ -1,25 +1,66 @@
 using Xunit;
 using eBill;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 
 namespace eBill.Tests
 {
-    public class BillTest
+    public class BillTest: IDisposable
     {
-        [Fact]
-        public void PassingTest()
-        {
-            Assert.Equal(4, Add(2, 2));
-        }
+    private IWebDriver driver;
+    private const string Url = "http://localhost:5000";
+
+       
+        public  BillTest()
+    {
+        // Set up the WebDriver (in this example, we're using Chrome)
+        driver = new ChromeDriver();
+    }
 
         [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
+        public void CheckIfHeaderElementExists()
+    {
+        // Navigate to the web page
+        driver.Navigate().GoToUrl(Url);
+        IWebElement element =  driver.FindElement(By.TagName ("h4"));
 
-        int Add(int x, int y)
-        {
-            return x + y;
-        }
+        // Assert that the element is not null, indicating that it exists
+        Assert.NotNull(element);
+        Assert.Equal("eBill", element.Text);
+
+    }
+
+        [Fact]
+        public void CheckIfCreateButtonElementExists()
+    {
+        // Navigate to the web page
+        driver.Navigate().GoToUrl(Url);
+        IWebElement element =  driver.FindElement(By.ClassName ("btn-primary"));
+
+        // Assert that the element is not null, indicating that it exists
+        Assert.NotNull(element);
+
+    }
+
+        [Fact]
+        public void checkIfAllBillsShowRightTitle()
+    {
+        // Navigate to the web page
+         driver.Navigate().GoToUrl("http://localhost:5000/Home/AllBills");
+        IWebElement element =  driver.FindElement(By.TagName ("h6"));
+
+        // Assert that the element is not null, indicating that it exists
+        Assert.NotNull(element);
+        Assert.Equal("Showing all bills", element.Text);
+
+    }
+
+        [Fact]
+        public void Dispose()
+    {
+        // Close the WebDriver when the test is finished
+        driver.Quit();
+    }
     }
 }
